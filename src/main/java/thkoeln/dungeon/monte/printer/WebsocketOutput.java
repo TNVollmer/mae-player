011d08d27086a16ddc9a60c30d9c6e55d21e6ed9
Stefan Bente <stefan.bente@th-koeln.de>
2023-01-11 18:13:17 +0100

    works now, but columns are way too wide

--- src/main/java/thkoeln/dungeon/monte/printer/WebsocketOutput.java
+++ src/main/java/thkoeln/dungeon/monte/printer/WebsocketOutput.java
@@ -36,32 +36,32 @@ public class WebsocketOutput implements OutputDevice {
 
     @Override
     public void header( String string ) {
-        stringBuffer.append( "<h2>" ).append( string ).append( "</h2>\n" );
+        stringBuffer.append( "<h2>" ).append( string ).append( "</h2>" );
     }
 
     @Override
     public void startBulletList() {
-        stringBuffer.append( "<ul>\n" );
+        stringBuffer.append( "<ul>" );
     }
 
     @Override
     public void endBulletList() {
-        stringBuffer.append( "</ul>\n" );
+        stringBuffer.append( "</ul>" );
     }
 
     @Override
     public void writeBulletItem( String string ) {
-        stringBuffer.append( "<li>" ).append( string ).append( "</li>\n" );
+        stringBuffer.append( "<li>" ).append( string ).append( "</li>" );
     }
 
     @Override
     public void startLine() {
-        stringBuffer.append( "<p>\n" );
+        stringBuffer.append( "<p>" );
     }
 
     @Override
     public void startLineIndent() {
-        stringBuffer.append( "<p>&nbsp;&nbsp;&nbsp;&nbsp;\n" );
+        stringBuffer.append( "<p>&nbsp;&nbsp;&nbsp;&nbsp;" );
     }
 
     @Override
@@ -71,41 +71,41 @@ public class WebsocketOutput implements OutputDevice {
 
     @Override
     public void endLine() {
-        stringBuffer.append( "</p>\n" );
+        stringBuffer.append( "</p>" );
     }
 
     @Override
     public void startColorFont( Color color ) {
         switch ( color ) {
-            case YELLOW: stringBuffer.append( "<span style=\"color:yellow;\">\n" ); break;
-            case RED: stringBuffer.append( "<span style=\"color:red;\">\n" ); break;
-            case BLUE: stringBuffer.append( "<span style=\"color:blue;\">\n" ); break;
-            case GREEN: stringBuffer.append( "<span style=\"color:green;\">\n" ); break;
-            case YELLOW_BRIGHT: stringBuffer.append( "<span style=\"color:yellow;font-weight:bold;\">\n" ); break;
-            case RED_BRIGHT: stringBuffer.append( "<span style=\"color:red;font-weight:bold;\">\n" ); break;
-            case BLUE_BRIGHT: stringBuffer.append( "<span style=\"color:blue;font-weight:bold;\">\n" ); break;
-            case GREEN_BRIGHT: stringBuffer.append( "<span style=\"color:green;font-weight:bold;\">\n" ); break;
+            case YELLOW: stringBuffer.append( "<span style=\"color:yellow;\">" ); break;
+            case RED: stringBuffer.append( "<span style=\"color:red;\">" ); break;
+            case BLUE: stringBuffer.append( "<span style=\"color:blue;\">" ); break;
+            case GREEN: stringBuffer.append( "<span style=\"color:green;\">" ); break;
+            case YELLOW_BRIGHT: stringBuffer.append( "<span style=\"color:yellow;font-weight:bold;\">" ); break;
+            case RED_BRIGHT: stringBuffer.append( "<span style=\"color:red;font-weight:bold;\">" ); break;
+            case BLUE_BRIGHT: stringBuffer.append( "<span style=\"color:blue;font-weight:bold;\">" ); break;
+            case GREEN_BRIGHT: stringBuffer.append( "<span style=\"color:green;font-weight:bold;\">" ); break;
             default:
         }
     }
 
     @Override
     public void endColorFont() {
-        stringBuffer.append( "</span>\n" );
+        stringBuffer.append( "</span>" );
     }
 
     // todo
     @Override
     public void startColorBackground(Color color) {
         switch ( color ) {
-            case YELLOW: stringBuffer.append( "<span style=\"background-color:yellow;\">\n" ); break;
-            case RED: stringBuffer.append( "<span style=\"background-color:red;\">\n" ); break;
-            case BLUE: stringBuffer.append( "<span style=\"background-color:blue;\">\n" ); break;
-            case GREEN: stringBuffer.append( "<span style=\"background-color:green;\">\n" ); break;
-            case YELLOW_BRIGHT: stringBuffer.append( "<span style=\"background-color:yellow;font-weight:bold;\">\n" ); break;
-            case RED_BRIGHT: stringBuffer.append( "<span style=\"background-color:red;font-weight:bold;\">\n" ); break;
-            case BLUE_BRIGHT: stringBuffer.append( "<span style=\"background-color:blue;font-weight:bold;\">\n" ); break;
-            case GREEN_BRIGHT: stringBuffer.append( "<span style=\"background-color:green;font-weight:bold;\">\n" ); break;
+            case YELLOW: stringBuffer.append( "<span style=\"background-color:yellow;\">" ); break;
+            case RED: stringBuffer.append( "<span style=\"background-color:red;\">" ); break;
+            case BLUE: stringBuffer.append( "<span style=\"background-color:blue;\">" ); break;
+            case GREEN: stringBuffer.append( "<span style=\"background-color:green;\">" ); break;
+            case YELLOW_BRIGHT: stringBuffer.append( "<span style=\"background-color:yellow;font-weight:bold;\">" ); break;
+            case RED_BRIGHT: stringBuffer.append( "<span style=\"background-color:red;font-weight:bold;\">" ); break;
+            case BLUE_BRIGHT: stringBuffer.append( "<span style=\"background-color:blue;font-weight:bold;\">" ); break;
+            case GREEN_BRIGHT: stringBuffer.append( "<span style=\"background-color:green;font-weight:bold;\">" ); break;
             default:
         }
     }
@@ -118,16 +118,17 @@ public class WebsocketOutput implements OutputDevice {
     @Override
     public void startMap( int numOfColumns ) {
         currentNumberOfColumns = numOfColumns;
-        stringBuffer.append( "<table>\n" ).append( "<thead>\n" ).append( "<tr>\n" ).append( "<th>&nbsp;</th>\n" );
+        stringBuffer.append( "<table>" ).append( "<thead>" ).
+                append( "<tr>" ).append( "<th width='50'>&nbsp;</th>" );
         for ( int columnNumber = 0; columnNumber < currentNumberOfColumns; columnNumber++ ) {
-            stringBuffer.append( "<th>" ).append( columnNumber ).append( "</th>\n" );
+            stringBuffer.append( "<th width='50'>" ).append( columnNumber ).append( "</th>" );
         }
-        stringBuffer.append( "</tr>\n" ).append( "</thead>\n" ).append( "<tbody>\n" );
+        stringBuffer.append( "</tr>" ).append( "</thead>" ).append( "<tbody>" );
     }
 
     @Override
     public void endMap() {
-        stringBuffer.append( "</tbody>\n" ).append( "</table>\n" );
+        stringBuffer.append( "</tbody>" ).append( "</table>" );
     }
 
 
@@ -150,11 +151,16 @@ public class WebsocketOutput implements OutputDevice {
 
     @Override
     public void endMapRow() {
-        for ( int compartmentNumber = 0; compartmentNumber < currentNumberOfCompartments; compartmentNumber++ ) {
-            for ( String[] cell : currentRowCells ) {
-                stringBuffer.append( "<td>" ).append( cell[compartmentNumber] ).append( "</td>\n" );
+        stringBuffer.append( "<tr>" );
+        for ( String[] cell : currentRowCells ) {
+            stringBuffer.append( "<td width='50'><table class='inner'><tbody class='inner'>" );
+            for ( int compartmentNumber = 0; compartmentNumber < currentNumberOfCompartments; compartmentNumber++ ) {
+                stringBuffer.append( "<tr class='inner'><td class='inner'>" ).
+                        append( cell[compartmentNumber] ).append( "</tr></td>" );
             }
+            stringBuffer.append( "</tbody></table></td>" );
         }
+        stringBuffer.append( "</tr>" );
     }
 
     @Override
