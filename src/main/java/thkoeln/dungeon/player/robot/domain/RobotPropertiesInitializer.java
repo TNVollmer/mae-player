d0f6ac85e4008f0fac812c973849c84fb903734d
Tom Vollmer <tom_nikolas.vollmer@smail.th-koeln.de>
2024-10-04 13:31:04 +0200

    added info logging and fixed upgrade order setting
    Signed-off-by: Tom Vollmer <tom_nikolas.vollmer@smail.th-koeln.de>

--- src/main/java/thkoeln/dungeon/player/robot/domain/RobotPropertiesInitializer.java
+++ src/main/java/thkoeln/dungeon/player/robot/domain/RobotPropertiesInitializer.java
@@ -1,6 +1,7 @@
 package thkoeln.dungeon.player.robot.domain;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import thkoeln.dungeon.player.core.domainprimitives.purchasing.CapabilityType;
@@ -9,6 +10,7 @@ import thkoeln.dungeon.player.core.domainprimitives.robot.RobotType;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class RobotPropertiesInitializer {
 
@@ -22,9 +24,17 @@ public class RobotPropertiesInitializer {
     @PostConstruct
     public void init() {
         RobotType.setDefaultType(RobotType.valueOf(robotProperties.getDefaultType()));
+        log.info("Set default robot type to: {}", RobotType.getDefaultType());
+
         RobotType.setMaxCounts(robotProperties.getMinerMaxCount(), robotProperties.getScoutMaxCount(), robotProperties.getWarriorMaxCount());
+        log.info("Set max count per robot type to: MINER: {}, SCOUT: {}, WARRIOR: {}",
+                robotProperties.getMinerMaxCount(), robotProperties.getScoutMaxCount(), robotProperties.getWarriorMaxCount());
         RobotType.setPercentages(robotProperties.getMinerPercentage(), robotProperties.getScoutPercentage(), robotProperties.getWarriorPercentage());
+        log.info("Set percentages per robot type to: MINER: {}, SCOUT: {}, WARRIOR: {}",
+                robotProperties.getMinerPercentage(), robotProperties.getScoutPercentage(), robotProperties.getWarriorPercentage());
         RobotType.setCreateAfter(robotProperties.getCreateMinersAfter(), robotProperties.getCreateScoutsAfter(), robotProperties.getCreateWarriorsAfter());
+        log.info("Set create after per robot type to: MINER: {}, SCOUT: {}, WARRIOR: {}",
+                robotProperties.getCreateMinersAfter(), robotProperties.getCreateScoutsAfter(), robotProperties.getCreateWarriorsAfter());
 
         List<CapabilityType> minerUpgradeOrderList = new ArrayList<>();
         List<CapabilityType> scoutUpgradeOrderList = new ArrayList<>();
@@ -35,14 +45,17 @@ public class RobotPropertiesInitializer {
         }
 
         for (String upgrade : robotProperties.getScoutUpgradeOrder().split(",")) {
-            minerUpgradeOrderList.add(CapabilityType.valueOf(upgrade));
+            scoutUpgradeOrderList.add(CapabilityType.valueOf(upgrade));
         }
 
         for (String upgrade : robotProperties.getWarriorUpgradeOrder().split(",")) {
-            minerUpgradeOrderList.add(CapabilityType.valueOf(upgrade));
+            warriorUpgradeOrderList.add(CapabilityType.valueOf(upgrade));
         }
 
         RobotType.setUpgradeOrder(minerUpgradeOrderList, scoutUpgradeOrderList, warriorUpgradeOrderList);
+        log.info("Upgrade Order for MINER: {}", minerUpgradeOrderList);
+        log.info("Upgrade Order for SCOUT: {}", scoutUpgradeOrderList);
+        log.info("Upgrade Order for WARRIOR: {}", warriorUpgradeOrderList);
     }
 
 }
