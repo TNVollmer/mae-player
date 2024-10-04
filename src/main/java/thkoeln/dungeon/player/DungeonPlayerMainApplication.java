b3ce78e63b1631f076c8208fd474c9955fc0a7d4
Tom Vollmer <tom_nikolas.vollmer@smail.th-koeln.de>
2024-10-04 12:31:04 +0200

    added info log to player start
    Signed-off-by: Tom Vollmer <tom_nikolas.vollmer@smail.th-koeln.de>

--- src/main/java/thkoeln/dungeon/player/DungeonPlayerMainApplication.java
+++ src/main/java/thkoeln/dungeon/player/DungeonPlayerMainApplication.java
@@ -1,5 +1,6 @@
 package thkoeln.dungeon.player;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +9,16 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@Slf4j
 @SpringBootApplication
 @EnableAsync
 public class DungeonPlayerMainApplication {
 
     public static void main(String[] args) {
+
+        log.info("Player-Tom");
+        log.info("Version: 1.0");
+
         SpringApplication.run(DungeonPlayerMainApplication.class, args);
     }
 
